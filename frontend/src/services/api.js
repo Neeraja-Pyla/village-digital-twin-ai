e3f7a3b3a97925596f3d7e946fcc6231847ab001
Neeraja-Pyla <pylaneerja@gmail.com>
2026-07-19 12:16:13 +0530

    Fixed Railway backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://127.0.0.1:8001",
+export default axios.create({
+  baseURL: "https://village-digital-twin-ai-production.up.railway.app",
 });
-
-export default api;
